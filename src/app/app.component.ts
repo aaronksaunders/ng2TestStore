3be3c2ca99e489a6533b74c46f3503515dc39ced
Aaron Saunders <aaron@clearlyinnovative.com>
2016-11-22 20:37:39 -0500

    fix merge errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,25 @@
-<<<<<<< HEAD
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import {FormGroup, FormControl, Validators} from '@angular/forms'
-=======
-import {Component} from '@angular/core';
-import {Store} from '@ngrx/store';
-import {Observable} from 'rxjs/Observable';
-import {FormGroup, FormBuilder, Validators} from '@angular/forms'
->>>>>>> origin/master
 
 
 /**
  *
  */
 export interface Person {
-    firstName: String,
-    lastName: String,
-    id: String,
+  firstName : String,
+  lastName : String,
+  id : String,
 }
 
 interface AppState {
-    /**
-     *
-     *
-     * @type {Array<String>}
-     * @memberOf AppState
-     */
-    peopleArray: Array<Person>;
+  /**
+   *
+   *
+   * @type {Array<String>}
+   * @memberOf AppState
+   */
+  peopleArray: Array<Person>;
 }
 
 /**
@@ -36,69 +29,84 @@ interface AppState {
  * @class AppComponent
  */
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
+export class AppComponent implements OnInit{
+  /**
+   *
+   *
+   * @memberOf AppComponent
+   */
+  title = 'app works!';
 
-export class AppComponent {
-    /**
-     *
-     *
-     * @memberOf AppComponent
-     */
-    title = 'app works!';
+  /**
+   *
+   *
+   * @memberOf AppComponent
+   */
+  peopleArray;
+  simpleForm: FormGroup;
 
-    /**
-     *
-     *
-     * @memberOf AppComponent
-     */
-    peopleArray;
-    simpleForm: FormGroup;
+  /**
+   * Creates an instance of AppComponent.
+   *
+   * @param {Store<AppState>} _store
+   *
+   * @memberOf AppComponent
+   */
+  constructor ( private _store: Store<AppState> ) {
+    this.peopleArray = _store.select('people');
+  }
+  /**
+   *
+   * @memberOf AppComponent
+   */
+  ngOnInit () {
+    this.simpleForm = new FormGroup({
+      'firstName': new FormControl('', [Validators.required, Validators.minLength(5)]),
+      'lastName': new FormControl('', [Validators.required, Validators.minLength(5)]),
+    });
+  }
+  /**
 
-    /**
-     * Creates an instance of AppComponent.
-     *
-     * @param {Store<AppState>} _store
-     *
-     * @memberOf AppComponent
-     */
-    constructor(private _store: Store<AppState>, private _fb: FormBuilder) {
-        this.peopleArray = _store.select('people');
-
-        this.simpleForm = _fb.group({
-            'firstName': [null, Validators.required, Validators.minLength(8)],
-            'lastName': [null, Validators.required, Validators.minLength(8)],
-        });
+   * @param {string} _controlName
+   *
+   * @memberOf AppComponent
+   */
+  errorHelper(_controlName:string) {
+    let control = this.simpleForm.controls[_controlName]
+    if (!control.pristine && control.errors) {
+      return control.errors
     }
+  }
+  /**
 
-    /**
-     *
-     * @param {any} _person
-     *
-     * @memberOf AppComponent
-     */
-    addPerson(_person) {
-        this
-            ._store
-            .dispatch({type: 'ADD_PERSON', payload: _person.value});
+   * @param {any} _person
+   *
+   * @memberOf AppComponent
+   */
+  addPerson(_person) {
+    this
+      ._store
+      .dispatch({type: 'ADD_PERSON', payload: _person.value});
 
-        // reset the form
-        this.simpleForm.reset();
+    // reset the form
+    this.simpleForm.reset();
 
-    }
+  }
 
 
-    /**
-     *
-     * @param {any} _person
-     *
-     * @memberOf AppComponent
-     */
-    removePerson(_person) {
-        this
-            ._store
-            .dispatch({type: 'REMOVE_PERSON', payload: _person});
-    }
+  /**
+   *
+   * @param {any} _person
+   *
+   * @memberOf AppComponent
+   */
+  removePerson(_person) {
+    this
+      ._store
+      .dispatch({type: 'REMOVE_PERSON', payload: _person});
+  }
 }
